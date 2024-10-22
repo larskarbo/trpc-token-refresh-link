0b5ec09761c4107879c8e4a829b8da6100e4984d
Alex / KATT <alex@tolahq.com>
2024-10-22 00:07:47 -0700

    add test, reduce deps

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,35 @@
-import type { Operation, TRPCLink } from '@trpc/client'
+import { type Operation, TRPCClientError, type TRPCLink } from '@trpc/client'
 import type { AnyRouter } from '@trpc/server'
 import { observable } from '@trpc/server/observable'
-import PQueue from 'p-queue'
-
-const queue = new PQueue({ concurrency: 1 })
 
 export type TokenRefreshProperties = {
 	tokenRefreshNeeded: (op: Operation) => boolean
 	fetchAccessToken: (op: Operation) => Promise<void>
 }
-
 export const tokenRefreshLink = <AppRouter extends AnyRouter>({
 	tokenRefreshNeeded,
 	fetchAccessToken,
 }: TokenRefreshProperties): TRPCLink<AppRouter> =>
 () => {
-	return ({ next, op }) => {
-		// each link needs to return an observable which propagates results
-		return observable((observer) => {
-			void queue.add(async () => {
-				const shouldRenew = tokenRefreshNeeded(op)
-				if (shouldRenew) {
-					// ok we need to refresh the token
-					await fetchAccessToken(op)
-				}
+	let refreshPromise: Promise<void> | undefined
+	return (options) => {
+		const { next, op } = options
 
-				next(op).subscribe({
-					next(value) {
-						observer.next(value)
-					},
-					error(error) {
-						observer.error(error)
-					},
-					complete() {
-						observer.complete()
-					},
-				})
+		return observable((observer) => {
+			if (!tokenRefreshNeeded(op)) {
+				return next(op).subscribe(observer)
+			}
+
+			refreshPromise ??= fetchAccessToken(op)
+
+			refreshPromise.then(() => {
+				next(op).subscribe(observer)
+			}).catch((error) => {
+				observer.error(TRPCClientError.from(error))
 			})
+				.finally(() => {
+					refreshPromise = undefined
+				})
 		})
 	}
 }
