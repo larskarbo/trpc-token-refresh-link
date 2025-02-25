a33d4f0b531aea331aa7ea85a25e11691168facc
Lars Karbo <10865165+larskarbo@users.noreply.github.com>
2025-02-25 12:44:40 +0000

    add proper test files

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+	test: {
+		environment: 'node',
+		include: ['**/*.test.ts'],
+		coverage: {
+			reporter: ['text', 'json', 'html'],
+		},
+	},
+})
